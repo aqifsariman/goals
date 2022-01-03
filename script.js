3b1744325d5940b23702d502a6e709651b307607
Aqif Sariman <79038951+aqifsariman@users.noreply.github.com>
2022-01-03 21:03:45 +0800

    Added files in

---
+++ script.js
@@ -0,0 +1,29 @@
+/* 2022 Goals Planning
+To present in pwpt deck - 2 Jan 2022
+
+Categorize as follows: 
+1. Anaqi
+2. Self
+3. Self 
+4. Partner
+5. Religion
+6. Health
+7. Finances
+8. School
+9. Habit
+10. Habit
+11. Wellness
+12. Hobby
+
+Guiding questions:
+What is the goal? How do you intend to achieve it? How long will you take to achieve it? */
+
+// let startText = document.createElement("div");
+// startText.innerHTML = "2022 GOALS PLANNING";
+// startText.setAttribute("id", "toptext");
+
+// let startOfPlan = () => {
+//   document.body.appendChild(startText);
+// };
+
+// startOfPlan();
